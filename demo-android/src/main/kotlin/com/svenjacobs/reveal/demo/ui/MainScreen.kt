e99479a8be851027dc896d11f05c4c9ff77a1c3d
Owen LeJeune <owen@distillmobile.com>
2022-12-16 12:51:38 -0500

    implement a more robust api for aligning overlay views

--- demo-android/src/main/kotlin/com/svenjacobs/reveal/demo/ui/MainScreen.kt
+++ demo-android/src/main/kotlin/com/svenjacobs/reveal/demo/ui/MainScreen.kt
@@ -1,39 +1,61 @@
 package com.svenjacobs.reveal.demo.ui
 
+import androidx.compose.foundation.interaction.MutableInteractionSource
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
+import androidx.compose.material3.BottomAppBar
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.CenterAlignedTopAppBar
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.NavigationBarItem
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.svenjacobs.reveal.HorizontalRevealOverlayAlignment
+import com.svenjacobs.reveal.HorizontalRevealOverlayAnchor
 import com.svenjacobs.reveal.Key
 import com.svenjacobs.reveal.Reveal
 import com.svenjacobs.reveal.RevealOverlayAlignment
 import com.svenjacobs.reveal.RevealOverlayScope
+import com.svenjacobs.reveal.RevealScope
 import com.svenjacobs.reveal.RevealShape
+import com.svenjacobs.reveal.VerticalRevealOverlayAlignment
+import com.svenjacobs.reveal.VerticalRevealOverlayAnchor
 import com.svenjacobs.reveal.demo.ui.theme.DemoTheme
 import com.svenjacobs.reveal.rememberRevealState
 import kotlin.time.Duration.Companion.seconds
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
 
-private enum class Keys { Fab, Explanation }
+private enum class Keys { Fab, Explanation, BarItem }
 
 @Composable
 @OptIn(ExperimentalMaterial3Api::class)
@@ -53,10 +75,16 @@ fun MainScreen(modifier: Modifier = Modifier) {
 			revealState = revealState,
 			onRevealableClick = { key ->
 				scope.launch {
-					if (key == Keys.Fab) {
-						revealState.reveal(Keys.Explanation)
-					} else {
-						revealState.hide()
+					when (key) {
+						Keys.Fab -> {
+							revealState.reveal(Keys.Explanation)
+						}
+						Keys.Explanation -> {
+							revealState.reveal(Keys.BarItem)
+						}
+						else -> {
+							revealState.hide()
+						}
 					}
 				}
 			},
@@ -69,6 +97,9 @@ fun MainScreen(modifier: Modifier = Modifier) {
 					CenterAlignedTopAppBar(
 						title = { Text("Reveal Demo") },
 					)
+				},
+				bottomBar = {
+					BottomNavBar()
 				},
 				floatingActionButton = {
 					FloatingActionButton(
@@ -112,6 +143,31 @@ fun MainScreen(modifier: Modifier = Modifier) {
 }
 
 @Composable
+fun RevealScope.BottomNavBar() {
+	var selectedIndex by remember { mutableStateOf(0) }
+	BottomAppBar {
+		for (i in 0 until 5) {
+			val m = if (i == 0) Modifier.revealable(Keys.BarItem) else Modifier
+			NavigationBarItem(
+				modifier = m,
+				selected = selectedIndex == i,
+				onClick = { selectedIndex = i },
+				interactionSource = MutableInteractionSource(),
+				icon = {
+					Icon(
+						imageVector = Icons.Filled.Add,
+						contentDescription = null
+					)
+				},
+				label = {
+					Text(text = "Item")
+				}
+			)
+		}
+	}
+}
+
+@Composable
 fun RevealOverlayScope.RevealOverlayContent(key: Key) {
 	when (key) {
 		Keys.Fab -> OverlayText(
@@ -121,6 +177,10 @@ fun RevealOverlayScope.RevealOverlayContent(key: Key) {
 		Keys.Explanation -> OverlayText(
 			modifier = Modifier.align(RevealOverlayAlignment.Bottom),
 			text = "Actually we already started. This was an example of the reveal effect.",
+		)
+		Keys.BarItem -> OverlayText(
+			modifier = Modifier.align(VerticalRevealOverlayAlignment.Top, VerticalRevealOverlayAnchor.ParentCenter),
+			text = "This is an example of aligning an overlay in the parent"
 		)
 	}
 }
--- reveal-core/src/main/kotlin/com/svenjacobs/reveal/RevealOverlay.kt
+++ reveal-core/src/main/kotlin/com/svenjacobs/reveal/RevealOverlay.kt
@@ -4,11 +4,28 @@ import androidx.compose.runtime.Immutable
 import androidx.compose.runtime.Stable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.layout.layout
+import androidx.compose.ui.platform.LocalConfiguration
 import androidx.compose.ui.unit.IntRect
 import androidx.compose.ui.unit.LayoutDirection
 
 enum class RevealOverlayAlignment {
-	Start, Top, End, Bottom, TopNoCenter, BottomNotCenter
+	Start, Top, End, Bottom
+}
+
+enum class VerticalRevealOverlayAlignment {
+	Top, Bottom
+}
+
+enum class VerticalRevealOverlayAnchor {
+	ParentStart, ParentEnd, ParentCenter, RevealableStart, RevealableEnd, RevealableCenter
+}
+
+enum class HorizontalRevealOverlayAlignment {
+	Start, End
+}
+
+enum class HorizontalRevealOverlayAnchor {
+	ParentTop, ParentCenter, ParentBottom, RevealableTop, RevealableCenter, RevealableBottom
 }
 
 /**
@@ -30,6 +47,12 @@ interface RevealOverlayScope {
 	 */
 	@Stable
 	fun Modifier.align(alignment: RevealOverlayAlignment): Modifier
+
+	@Stable
+	fun Modifier.align(verticalAlignment: VerticalRevealOverlayAlignment, anchor: VerticalRevealOverlayAnchor = VerticalRevealOverlayAnchor.RevealableCenter): Modifier
+
+	@Stable
+	fun Modifier.align(horizontalAlignment: HorizontalRevealOverlayAlignment, anchor: HorizontalRevealOverlayAnchor = HorizontalRevealOverlayAnchor.RevealableCenter): Modifier
 }
 
 internal class RevealOverlayScopeInstance(
@@ -64,11 +87,6 @@ internal class RevealOverlayScopeInstance(
 							x = horizontalCenterX,
 							y = revealRect.top - placeable.height,
 						)
-					RevealOverlayAlignment.TopNoCenter ->
-						placeable.place(
-							x = 0,
-							y = revealRect.top - placeable.height
-						)
 					RevealOverlayAlignment.End ->
 						placeable.place(
 							x = revealRect.right,
@@ -79,13 +97,223 @@ internal class RevealOverlayScopeInstance(
 							x = horizontalCenterX,
 							y = revealRect.bottom,
 						)
-					RevealOverlayAlignment.BottomNotCenter ->
+				}
+			}
+		},
+	)
+
+	override fun Modifier.align(
+		verticalAlignment: VerticalRevealOverlayAlignment,
+		anchor: VerticalRevealOverlayAnchor
+	): Modifier = this.then(
+		Modifier.layout { measurable, constraints ->
+			val placeable = measurable.measure(constraints)
+			layout(constraints.maxWidth, constraints.maxHeight) {
+				val actualAnchor = when {
+					layoutDirection == LayoutDirection.Rtl
+						&& anchor == VerticalRevealOverlayAnchor.ParentStart -> VerticalRevealOverlayAnchor.ParentEnd
+					layoutDirection == LayoutDirection.Rtl
+						&& anchor == VerticalRevealOverlayAnchor.ParentEnd -> VerticalRevealOverlayAnchor.ParentStart
+					layoutDirection == LayoutDirection.Rtl
+						&& anchor == VerticalRevealOverlayAnchor.RevealableStart -> VerticalRevealOverlayAnchor.RevealableEnd
+					layoutDirection == LayoutDirection.Rtl
+						&& anchor == VerticalRevealOverlayAnchor.RevealableEnd -> VerticalRevealOverlayAnchor.RevealableStart
+					else -> anchor
+				}
+
+				when {
+					verticalAlignment == VerticalRevealOverlayAlignment.Top
+						&& actualAnchor == VerticalRevealOverlayAnchor.ParentStart -> {
+							placeable.place(
+								x = 0,
+								y = revealRect.top - placeable.height
+							)
+						}
+					verticalAlignment == VerticalRevealOverlayAlignment.Top
+						&& actualAnchor == VerticalRevealOverlayAnchor.ParentCenter -> {
+							placeable.place(
+								x = (constraints.maxWidth / 2) - (placeable.width / 2),
+								y = revealRect.top - placeable.height
+							)
+						}
+					verticalAlignment == VerticalRevealOverlayAlignment.Top
+						&& actualAnchor == VerticalRevealOverlayAnchor.ParentEnd -> {
+						placeable.place(
+							x = constraints.maxWidth - placeable.width,
+							y = revealRect.top - placeable.height
+						)
+					}
+					verticalAlignment == VerticalRevealOverlayAlignment.Top
+						&& actualAnchor == VerticalRevealOverlayAnchor.RevealableStart -> {
+						placeable.place(
+							x = revealRect.left,
+							y = revealRect.top - placeable.height
+						)
+					}
+					verticalAlignment == VerticalRevealOverlayAlignment.Top
+						&& actualAnchor == VerticalRevealOverlayAnchor.RevealableCenter -> {
+						placeable.place(
+							x = revealRect.left + (revealRect.width - placeable.width) / 2,
+							y = revealRect.top - placeable.height
+						)
+					}
+					verticalAlignment == VerticalRevealOverlayAlignment.Top
+						&& actualAnchor == VerticalRevealOverlayAnchor.RevealableEnd -> {
+						placeable.place(
+							x = revealRect.right,
+							y = revealRect.top - placeable.height
+						)
+					}
+					verticalAlignment == VerticalRevealOverlayAlignment.Bottom
+						&& actualAnchor == VerticalRevealOverlayAnchor.ParentStart -> {
 						placeable.place(
 							x = 0,
 							y = revealRect.bottom
 						)
+					}
+					verticalAlignment == VerticalRevealOverlayAlignment.Bottom
+						&& actualAnchor == VerticalRevealOverlayAnchor.ParentCenter -> {
+						placeable.place(
+							x = (constraints.maxWidth / 2) - (placeable.width / 2),
+							y = revealRect.bottom
+						)
+					}
+					verticalAlignment == VerticalRevealOverlayAlignment.Bottom
+						&& actualAnchor == VerticalRevealOverlayAnchor.ParentEnd -> {
+						placeable.place(
+							x = constraints.maxWidth - placeable.width,
+							y = revealRect.bottom
+						)
+					}
+					verticalAlignment == VerticalRevealOverlayAlignment.Bottom
+						&& actualAnchor == VerticalRevealOverlayAnchor.RevealableStart -> {
+						placeable.place(
+							x = revealRect.left,
+							y = revealRect.bottom
+						)
+					}
+					verticalAlignment == VerticalRevealOverlayAlignment.Bottom
+						&& actualAnchor == VerticalRevealOverlayAnchor.RevealableCenter -> {
+						placeable.place(
+							x = revealRect.left + (revealRect.width - placeable.width) / 2,
+							y = revealRect.bottom
+						)
+					}
+					verticalAlignment == VerticalRevealOverlayAlignment.Bottom
+						&& actualAnchor == VerticalRevealOverlayAnchor.RevealableEnd -> {
+						placeable.place(
+							x = revealRect.right,
+							y = revealRect.bottom
+						)
+					}
 				}
 			}
-		},
+		}
+	)
+
+	override fun Modifier.align(
+		horizontalAlignment: HorizontalRevealOverlayAlignment,
+		anchor: HorizontalRevealOverlayAnchor
+	): Modifier = this.then(
+		Modifier.layout { measurable, constraints ->
+			val placeable = measurable.measure(constraints)
+			layout(constraints.maxWidth, constraints.maxHeight) {
+				val actualAlignment = when {
+					layoutDirection == LayoutDirection.Rtl
+						&& horizontalAlignment == HorizontalRevealOverlayAlignment.Start -> HorizontalRevealOverlayAlignment.End
+					layoutDirection == LayoutDirection.Rtl
+						&& horizontalAlignment == HorizontalRevealOverlayAlignment.End -> HorizontalRevealOverlayAlignment.Start
+					else -> horizontalAlignment
+				}
+
+				when {
+					actualAlignment == HorizontalRevealOverlayAlignment.Start
+						&& anchor == HorizontalRevealOverlayAnchor.ParentTop -> {
+							placeable.place(
+								x = revealRect.left - placeable.width,
+								y = 0
+							)
+						}
+					actualAlignment == HorizontalRevealOverlayAlignment.Start
+						&& anchor == HorizontalRevealOverlayAnchor.ParentCenter -> {
+							placeable.place(
+								x = revealRect.left - placeable.width,
+								y = (constraints.maxHeight / 2) - (placeable.height / 2)
+							)
+						}
+					actualAlignment == HorizontalRevealOverlayAlignment.Start
+						&& anchor == HorizontalRevealOverlayAnchor.ParentBottom -> {
+							placeable.place(
+								x = revealRect.left - placeable.width,
+								y = constraints.maxHeight - (placeable.height)
+							)
+						}
+					actualAlignment == HorizontalRevealOverlayAlignment.Start
+						&& anchor == HorizontalRevealOverlayAnchor.RevealableTop -> {
+							placeable.place(
+								x = revealRect.left - placeable.width,
+								y = revealRect.top - placeable.height
+							)
+						}
+					actualAlignment == HorizontalRevealOverlayAlignment.Start
+						&& anchor == HorizontalRevealOverlayAnchor.RevealableCenter -> {
+							placeable.place(
+								x = revealRect.left - placeable.width,
+								y = revealRect.top + (revealRect.height - placeable.height) / 2
+							)
+						}
+					actualAlignment == HorizontalRevealOverlayAlignment.Start
+						&& anchor == HorizontalRevealOverlayAnchor.RevealableBottom -> {
+							placeable.place(
+								x = revealRect.left - placeable.width,
+								y = revealRect.bottom
+							)
+						}
+
+					actualAlignment == HorizontalRevealOverlayAlignment.End
+						&& anchor == HorizontalRevealOverlayAnchor.ParentTop -> {
+						placeable.place(
+							x = revealRect.right,
+							y = 0
+						)
+					}
+					actualAlignment == HorizontalRevealOverlayAlignment.End
+						&& anchor == HorizontalRevealOverlayAnchor.ParentCenter -> {
+						placeable.place(
+							x = revealRect.right,
+							y = (constraints.maxHeight / 2) - (placeable.height / 2)
+						)
+					}
+					actualAlignment == HorizontalRevealOverlayAlignment.End
+						&& anchor == HorizontalRevealOverlayAnchor.ParentBottom -> {
+						placeable.place(
+							x = revealRect.right,
+							y = constraints.maxHeight - (placeable.height)
+						)
+					}
+					actualAlignment == HorizontalRevealOverlayAlignment.End
+						&& anchor == HorizontalRevealOverlayAnchor.RevealableTop -> {
+						placeable.place(
+							x = revealRect.right,
+							y = revealRect.top - placeable.height
+						)
+					}
+					actualAlignment == HorizontalRevealOverlayAlignment.End
+						&& anchor == HorizontalRevealOverlayAnchor.RevealableCenter -> {
+						placeable.place(
+							x = revealRect.right,
+							y = revealRect.top + (revealRect.height - placeable.height) / 2
+						)
+					}
+					actualAlignment == HorizontalRevealOverlayAlignment.End
+						&& anchor == HorizontalRevealOverlayAnchor.RevealableBottom -> {
+						placeable.place(
+							x = revealRect.right,
+							y = revealRect.bottom
+						)
+					}
+				}
+			}
+		}
 	)
 }
