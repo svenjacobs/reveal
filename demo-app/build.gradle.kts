b85bcc2ce2f8328d24d3eb27f2bf798e650400be
Sven Jacobs <github@svenjacobs.com>
2024-01-21 13:46:22 +0100

    chore: Update dependencies

--- demo-app/build.gradle.kts
+++ demo-app/build.gradle.kts
@@ -1,12 +1,12 @@
 import com.github.benmanes.gradle.versions.updates.DependencyUpdatesTask
 
 plugins {
-	id("com.android.application").version("8.1.2").apply(false)
-	id("com.android.library").version("8.1.2").apply(false)
-	kotlin("android").version("1.9.10").apply(false)
-	kotlin("multiplatform").version("1.9.10").apply(false)
-	id("org.jetbrains.compose").version("1.5.2").apply(false)
-	id("com.github.ben-manes.versions").version("0.48.0")
+	id("com.android.application").version("8.2.1").apply(false)
+	id("com.android.library").version("8.2.1").apply(false)
+	kotlin("android").version("1.9.21").apply(false)
+	kotlin("multiplatform").version("1.9.21").apply(false)
+	id("org.jetbrains.compose").version("1.5.11").apply(false)
+	id("com.github.ben-manes.versions").version("0.50.0")
 }
 
 tasks.register("clean", Delete::class) {
