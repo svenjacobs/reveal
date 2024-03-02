2aa9c1987eb56c4b13e4d584c91ab1a3ca938843
Sven Jacobs <github@svenjacobs.com>
2024-03-02 19:19:05 +0100

    chore: Update dependencies (JetBrains Compose 1.6.0)

--- convention-plugins/src/main/kotlin/convention.multiplatform.gradle.kts
+++ convention-plugins/src/main/kotlin/convention.multiplatform.gradle.kts
@@ -1,38 +1,40 @@
-//import org.jetbrains.kotlin.gradle.ExperimentalKotlinGradlePluginApi
-//
-//plugins {
-//	kotlin("multiplatform")
-//	id("org.jetbrains.compose")
-//}
-//
-//val baseName: String by extra
-//
-//@OptIn(ExperimentalKotlinGradlePluginApi::class)
-//kotlin {
-//	jvm("desktop")
-//
-//	androidTarget {
-//		compilations.all {
-//			kotlinOptions {
-//				jvmTarget = "11"
-//			}
-//		}
-//		publishLibraryVariants("release")
-//	}
-//
-//	listOf(
-//		iosX64(),
-//		iosArm64(),
-//		iosSimulatorArm64(),
-//	).forEach {
-//		it.binaries.framework {
-//			this.baseName = baseName
-//		}
-//	}
-//
-//	js(IR) {
-//		browser()
-//	}
-//
-//	explicitApi()
-//}
+import org.jetbrains.kotlin.gradle.ExperimentalKotlinGradlePluginApi
+
+plugins {
+	kotlin("multiplatform")
+	id("org.jetbrains.compose")
+}
+
+val baseName: String by extra
+
+@OptIn(ExperimentalKotlinGradlePluginApi::class)
+kotlin {
+	applyDefaultHierarchyTemplate()
+
+	jvm("desktop")
+
+	androidTarget {
+		compilations.all {
+			kotlinOptions {
+				jvmTarget = "11"
+			}
+		}
+		publishLibraryVariants("release")
+	}
+
+	listOf(
+		iosX64(),
+		iosArm64(),
+		iosSimulatorArm64(),
+	).forEach {
+		it.binaries.framework {
+			this.baseName = baseName
+		}
+	}
+
+	js(IR) {
+		browser()
+	}
+
+	explicitApi()
+}
