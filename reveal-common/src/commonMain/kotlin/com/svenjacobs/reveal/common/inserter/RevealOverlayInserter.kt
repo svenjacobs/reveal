7e9133b2cd390f7a7642164fe5fd821cc9ce8393
Sven Jacobs <github@svenjacobs.com>
2024-03-02 19:47:56 +0100

    chore: Add @Stable to RevealOverlayInserter

--- reveal-common/src/commonMain/kotlin/com/svenjacobs/reveal/common/inserter/RevealOverlayInserter.kt
+++ reveal-common/src/commonMain/kotlin/com/svenjacobs/reveal/common/inserter/RevealOverlayInserter.kt
@@ -1,11 +1,13 @@
 package com.svenjacobs.reveal.common.inserter
 
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.Stable
 import androidx.compose.ui.unit.DpOffset
 
 /**
  * Defines a strategy of how to insert the overlay into the composition.
  */
+@Stable
 public interface RevealOverlayInserter {
 
 	/**
