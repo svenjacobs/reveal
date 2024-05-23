75e76cba708f49fb66ac162d148f4b0bfa96da58
Sven Jacobs <github@svenjacobs.com>
2024-05-23 08:30:55 +0200

    chore: update dependencies of demo app (#128)

--- demo-app/androidApp/build.gradle.kts
+++ demo-app/androidApp/build.gradle.kts
@@ -17,7 +17,7 @@ android {
 		compose = true
 	}
 	composeOptions {
-		kotlinCompilerExtensionVersion = "1.5.10"
+		kotlinCompilerExtensionVersion = "1.5.14"
 	}
 	packaging {
 		resources {
@@ -40,12 +40,12 @@ android {
 
 dependencies {
 	implementation(project(":shared"))
-	implementation(platform("androidx.compose:compose-bom:2024.02.01"))
+	implementation(platform("androidx.compose:compose-bom:2024.05.00"))
 	implementation("androidx.compose.ui:ui")
 	implementation("androidx.compose.ui:ui-tooling")
 	implementation("androidx.compose.ui:ui-tooling-preview")
 	implementation("androidx.compose.foundation:foundation")
 	implementation("androidx.compose.material3:material3")
-	implementation("androidx.activity:activity-compose:1.8.2")
-	implementation("androidx.core:core-ktx:1.12.0")
+	implementation("androidx.activity:activity-compose:1.9.0")
+	implementation("androidx.core:core-ktx:1.13.1")
 }
