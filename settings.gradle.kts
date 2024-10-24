611aa34c709f9f0c7fb324907c09743c674a8e01
Sven Jacobs <github@svenjacobs.com>
2024-10-24 18:47:39 +0200

    chore: add foojay resolver Gradle plugin (#178)

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -12,6 +12,10 @@ pluginManagement {
 	includeBuild("convention-plugins")
 }
 
+plugins {
+	id("org.gradle.toolchains.foojay-resolver-convention") version "0.8.0"
+}
+
 dependencyResolutionManagement {
 	repositories {
 		google()
