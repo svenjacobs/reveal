8e511a1b86d25c03aaa1658290abd57e6e1b090e
Sven Jacobs <github@svenjacobs.com>
2025-01-18 18:06:25 +0100

    chore: update dependencies

--- demo-app/androidApp/build.gradle.kts
+++ demo-app/androidApp/build.gradle.kts
@@ -6,11 +6,11 @@ plugins {
 
 android {
 	namespace = "com.svenjacobs.reveal.demo.android"
-	compileSdk = 34
+	compileSdk = 35
 	defaultConfig {
 		applicationId = "com.svenjacobs.reveal.demo.android"
 		minSdk = 21
-		targetSdk = 34
+		targetSdk = 35
 		versionCode = 1
 		versionName = "1.0"
 	}
